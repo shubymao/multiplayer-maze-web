722cdb421fa695991f90c511d2f36963e1c4c8b8
shuby mao <le-li.mao@mail.mcgill.ca>
2021-04-21 21:53:37 -0400

    added photo and fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GenerationDemo from './pages/GenerateDemo';
-import Maze from './pages/Maze';
 import MultiPlayerMaze from './pages/MultiplayerMaze';
 
 function App(): JSX.Element {
