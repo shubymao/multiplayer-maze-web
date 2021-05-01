d5a9c0f5155379e8af60478d2f642a5d6f66f264
shuby mao <le-li.mao@mail.mcgill.ca>
2021-04-30 20:43:00 -0400

    changed url

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,9 @@ import { Control, Link } from './type';
 const LINKS: Array<Link> = [
   { name: 'Home', url: 'https://shuby-mao.web.app/' },
   { name: 'Project Page', url: 'https://shuby-mao.web.app/projects/web-multiplayer-maze' },
-  { name: 'Multiplayer Maze', url: 'online' },
-  { name: 'Generator Demo', url: 'generator' },
-  { name: 'Offline Maze', url: './' }
+  { name: 'Multiplayer Maze', url: '/online' },
+  { name: 'Generator Demo', url: '/generator' },
+  { name: 'Offline Maze', url: '/' }
 ];
 
 export const IDLE_CONTROL: Control = { magnitude: 0, angle: 0 };
